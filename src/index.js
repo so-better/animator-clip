e6ec6a8222532d6954c8f3d4173c5690c2c68728
lovelin0523 <18356201582@163.com>
2021-03-18 16:54:09 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const Animator = require('./animator')
 
 const Clip = require('./clip')
 
-const version = require('../package.json').version;
-const github = require('../package.json').github;
-console.log('%c感谢使用animator-clip，当前版本：%c v'+version+'\n%canimator-clip完全由个人开发，如果你觉得还不错的话，欢迎前往github给个star，感谢！\ngithub地址：%c'+github,'color:#808080;','color:#008a00','color:#808080;','color:#008a00');
+const packages = require('../package.json');
+
+console.log('%c感谢使用'+packages.name+'，当前版本：%c v'+packages.version+'\n%c'+packages.name+'完全由个人开发，如果你觉得还不错的话，欢迎前往github给个star，感谢！\ngithub地址：%c'+packages.github,'color:#808080;','color:#008a00','color:#808080;','color:#008a00');
 
 module.exports = {
 	Animator,Clip
